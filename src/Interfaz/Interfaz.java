22e1cd21086b780331cd3799f943f0d79bbd8263
AToala <aitoala@espol.edu.ec>
2020-07-03 21:42:28 -0500

    Creación del menú principal.

--- src/Interfaz/Interfaz.java
+++ src/Interfaz/Interfaz.java
@@ -4,21 +4,54 @@
  * and open the template in the editor.
  */
 package Interfaz;
-
+import java.util.Scanner;
 /**
  *
  * @author AToala
  */
 public class Interfaz {
 
-    /**
-     * @param args the command line arguments
-     */
+    public Scanner sc;
+    
+    public Interfaz(){
+        sc = new Scanner(System.in);
+    }
+    public void menu(){
+        System.out.println("Bienvenido a AgroStoreNU");
+        String op="";
+        while(!op.equals("3")){
+            System.out.println("------------------------------------------");
+            System.out.println("1. Iniciar Sesion");
+            System.out.println("2. Registrar Usuario");
+            System.out.println("3. Salir");
+            System.out.println("------------------------------------------");
+            System.out.print("Ingrese una opcion: ");
+
+            op = sc.nextLine();
+            
+            //hace una opcion de acuerdo a lo ingresado por el usuario
+            switch(op){
+                case "1":
+                    //Menu INICIAR SESION
+                    
+                    continue;
+                case "2":
+                    //Menu REGISTRAR USUARIO
+                    
+                    continue;
+                case "3":
+                    System.out.println("Adios");
+                    break;
+                default:
+                    System.out.println("Opcion invalida");
+                    break;
+            }
+        }
+    }
     public static void main(String[] args) {
-        // TODO code application logic here
-        System.out.println("ProyectoX");
-        System.out.println("POO es RP");
-        System.out.println("Hola miss");
+        Interfaz ui = new Interfaz();
+        ui.menu();
+        
     }
     
 }
